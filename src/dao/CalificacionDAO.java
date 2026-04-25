b997e31841efbc63c7e23480016664b369b52aea
iSAACd65-dev <colindresd65@gmail.com>
2026-04-25 00:55:42 -0600

    Update CalificacionDAO.java

--- src/dao/CalificacionDAO.java
+++ src/dao/CalificacionDAO.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class CalificacionDAO {
 
     public static void calificar() {
-        Scanner sc = new Scanner(System.in);
+       Scanner sc = new Scanner(System.in);
 
         try {
             Connection con = Conexion.conectar();
@@ -21,20 +21,48 @@ public class CalificacionDAO {
             System.out.print("Ingresa una calificacion del 1 al 5: ");
             int calificacion = sc.nextInt();
 
-            PreparedStatement ps = con.prepareStatement(
-                "INSERT INTO Calificacion (Cod_Usuario, Cod_pelicula, Calificacion) VALUES (?, ?, ?)"
+          
+            PreparedStatement check = con.prepareStatement(
+                "SELECT * FROM Calificacion WHERE Cod_Usuario = ? AND Cod_pelicula = ?"
             );
+            check.setInt(1, idUsuario);
+            check.setInt(2, codPelicula);
 
-            ps.setInt(1, idUsuario);
-            ps.setInt(2, codPelicula);
-            ps.setInt(3, calificacion);
+            ResultSet rs = check.executeQuery();
 
-            ps.executeUpdate();
+            if (rs.next()) {
+                
+                PreparedStatement update = con.prepareStatement(
+                    "UPDATE Calificacion SET Calificacion = ? WHERE Cod_Usuario = ? AND Cod_pelicula = ?"
+                );
+                update.setInt(1, calificacion);
+                update.setInt(2, idUsuario);
+                update.setInt(3, codPelicula);
+                update.executeUpdate();
 
-            System.out.println(" Se ha registrado tu calificacion");
+                System.out.println(" Calificación actualizada correctamente");
+
+            } else {
+                
+                PreparedStatement insert = con.prepareStatement(
+                    "INSERT INTO Calificacion (Cod_Usuario, Cod_pelicula, Calificacion) VALUES (?, ?, ?)"
+                );
+                insert.setInt(1, idUsuario);
+                insert.setInt(2, codPelicula);
+                insert.setInt(3, calificacion);
+                insert.executeUpdate();
+
+                System.out.println(" Calificación registrada correctamente");
+            }
+
+            
+            rs.close();
+            check.close();
+            con.close();
 
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
 }
+
